aa4d86db8745a13724b10524553b8e2d6181c726
tay52636 <phuongtay52636@gmail.com>
2025-03-28 01:26:18 +0700

    change Form Login ,sigup mergeForm

---
+++ src/views/Auth/LoginAndSignUp.php
@@ -0,0 +1,58 @@
+<div class="qwix-container">
+    <div class="qwix-box" id="qwix-box">
+        <div class="vorn-form mith-signup">
+            <form action="#">
+                <h1 class="login-title">Tạo tài Khoản</h1>
+                <div class="jynx-social">
+                    <a href="#" class="social"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="social"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-tiktok"></i></a>
+                </div>
+                <input type="text" placeholder="Tên tài khoản của bạn" />
+                <div class="zylith-pass-wrapper">
+                    <input type="password" placeholder="Nhập mật khẩu của bạn" class="password-input" />
+                    <i class="fas fa-eye kwez-eye toggle-password"></i>
+                </div>
+                <div class="zylith-pass-wrapper">
+                    <input type="password" placeholder="Nhập lại mật khẩu" class="password-input" />
+                    <i class="fas fa-eye kwez-eye toggle-password"></i>
+                </div>
+                <button>Đăng ký</button>
+            </form>
+        </div>
+        <div class="vorn-form lyth-login">
+            <form action="#">
+                <h1 class="login-title">Đăng nhập</h1>
+                <div class="jynx-social">
+                    <a href="#" class="social"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="social"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social"><i class="fab fa-tiktok"></i></a>
+                </div>
+                <input type="text" placeholder="Tên tài khoản của bạn" />
+                <div class="zylith-pass-wrapper">
+                    <input type="password" placeholder="Nhập mật khẩu của bạn" class="password-input" />
+                    <i class="fas fa-eye kwez-eye toggle-password"></i>
+                </div>
+                <a href="#">Forgot your password?</a>
+                <button>Đăng nhập</button>
+            </form>
+        </div>
+        <div class="zeth-overlay">
+            <div class="klyth-slide">
+                <div class="plyth-panel xorn-left">
+                    <h1>Chào mừng bạn trở lại!!</h1>
+                    <p>Đã có tài khoản vui lòng bấm đăng nhập</p>
+                    <button class="xyth-ghost" id="lyth-login-btn">Đăng nhập</button>
+                </div>
+                <div class="plyth-panel yith-right">
+
+                    <h1>Chào mừng bạn trở lại!!</h1>
+                    <p>Nếu chưa có tài khoản vui lòng bấm vào đăng ký</p>
+                    <button class="xyth-ghost" id="mith-signup-btn">Đăng ký</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/views/Pages/Home.php
+++ src/views/Pages/Home.php
@@ -1,25 +1,29 @@
 <?php
-$isLogin  = isset($_GET['login']) && $_GET['login'] == 'true';
-$isSignUp = isset($_GET['signup']) && $_GET['signup'] == 'true';
+// Kiểm tra tham số trên URL để hiển thị nội dung phù hợp
+$page = isset($_GET['login']) ? 'login' : 'home';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include '../config/head.php'; ?>
 
 <body>
     <?php include '../layout/includes/Header.php'; ?>
+
     <div class="main-content">
-        <?php if ($isLogin): ?>
-            <?php include '../Auth/Login.php'; ?>
-        <?php elseif ($isSignUp): ?>
-            <?php include '../Auth/SignUp.php'; ?>
-        <?php else: ?>
-            <?php include '../Components/Banner/Banner.php'; ?>
-            <?php include '../Components/Products/Products.php'; ?>
-            <?php include '../Components/Feature/Feature.php'; ?>
-        <?php endif; ?>
+        <?php
+        if ($page === 'login') {
+            include '../Auth/LoginAndSignUp.php';
+        } else {
+            include '../Components/Banner/Banner.php';
+            include '../Components/Products/Products.php';
+            include '../Components/Feature/Feature.php';
+        }
+        ?>
     </div>
+
     <?php include '../config/script.php'; ?>
+    <?php include '../layout/includes/Footer.php'; ?>
 </body>
 
 </html>
